fddf0be4e2cc07332e4ca4fa586b795f1b7966e3
scripthound <grinkiko@gmail.com>
2022-08-31 18:07:38 +0300

    [VERSIONS] added note about spark master version compatibility

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A minimal configuration for spark interaction from scala
+
+For proper work scala and java version on Spark master must match with ones
+on client application.
+
+Also, launching Spark master from start-master.sh -h localhost -p 7077 seems
+enough
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,16 @@
 import org.apache.spark.sql.SparkSession
 
-object Main extends App {
-  val spark = SparkSession.builder()
-    .appName("MySparkApp")
-    .master("spark://localhost:7077")
-    .getOrCreate()
-  println(spark)
+object Main {
+  def main (args: Array[String]) = {
+    val logfile = "README.md"
+    val spark = SparkSession
+      .builder
+      .appName("MyApp")
+      .config("spark.master", "spark://localhost:7077")
+      .getOrCreate()
+    val logData = spark.read.textFile(logfile)
+    val numAs = logData.filter(line => line.contains("a")).count()
+    println(numAs)
+    spark.stop()
+  }
 }
